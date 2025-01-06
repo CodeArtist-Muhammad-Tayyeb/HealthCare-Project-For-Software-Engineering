7edfd7b9f29e60d624ee12f139e9a8579b578e6c
CodeArtist-Muhammad-Tayyeb <22jzbcs0223@uetpeshawar.edu.pk>
2025-01-06 09:53:23 +0500

    update

--- JS/doctors.js
+++ JS/doctors.js
@@ -20,65 +20,71 @@ function createCard({ image, title, description, moreLink, speclization }) {
       title: 'Cardiologist',
       description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum',
       moreLink: './cardiology-details.html', // Link for "Read More..."
-      speclization: 'MBBS'
+      specialization: 'MBBS'
     },
     {
       image: './asset/doc-2.jpg',
       title: 'Neurologist',
       description: 'Exploring the complexities of the human brain and nervous system',
       moreLink: './neurology-details.html', // Link for "Read More..."
-      speclization: 'Cardiologist'
+      specialization: 'Neurology'
     },
     {
       image: './asset/doc-6.jpg',
-      title: 'Orthopedics',
+      title: 'Orthopedist',
       description: 'Specializing in the treatment of bones and joints',
       moreLink: './orthopedics-details.html', // Link for "Read More..."
-      speclization: 'Orthopedics'
+      specialization: 'Orthopedics'
     },
     {
-        image: './asset/doc-6.jpg',
-        title: 'Cardiologist',
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum',
-        moreLink: './cardiology-details.html', // Link for "Read More..."
-        speclization: 'MBBS'
-      },
-      {
-        image: './asset/doc-2.jpg',
-        title: 'Neurologist',
-        description: 'Exploring the complexities of the human brain and nervous system',
-        moreLink: './neurology-details.html', // Link for "Read More..."
-        speclization: 'Cardiologist'
-      },
-      {
-        image: './asset/doc-6.jpg',
-        title: 'Orthopedics',
-        description: 'Specializing in the treatment of bones and joints',
-        moreLink: './orthopedics-details.html', // Link for "Read More..."
-        speclization: 'Orthopedics'
-      }
-      ,
+      image: './asset/doc-2.jpg',
+      title: 'Dermatologist',
+      description: 'Specializing in skin, hair, and nail conditions',
+      moreLink: './dermatology-details.html', // Link for "Read More..."
+      specialization: 'Dermatology'
+    },
     {
-        image: './asset/doc-6.jpg',
-        title: 'Cardiologist',
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum',
-        moreLink: './cardiology-details.html', // Link for "Read More..."
-        speclization: 'MBBS'
-      },
-      {
-        image: './asset/doc-2.jpg',
-        title: 'Neurologist',
-        description: 'Exploring the complexities of the human brain and nervous system',
-        moreLink: './neurology-details.html', // Link for "Read More..."
-        speclization: 'Cardiologist'
-      },
-      {
-        image: './asset/doc-6.jpg',
-        title: 'Orthopedics',
-        description: 'Specializing in the treatment of bones and joints',
-        moreLink: './orthopedics-details.html', // Link for "Read More..."
-        speclization: 'Orthopedics'
-      }
+      image: './asset/doc-6.jpg',
+      title: 'Pediatrician',
+      description: 'Providing specialized care for infants and children',
+      moreLink: './pediatrics-details.html', // Link for "Read More..."
+      specialization: 'Pediatrics'
+    },
+    {
+      image: './asset/doc-2.jpg',
+      title: 'Endocrinologist',
+      description: 'Treating hormonal imbalances and related disorders',
+      moreLink: './endocrinology-details.html', // Link for "Read More..."
+      specialization: 'Endocrinology'
+    },
+    {
+      image: './asset/doc-6.jpg',
+      title: 'Oncologist',
+      description: 'Specialized in cancer diagnosis and treatment',
+      moreLink: './oncology-details.html', // Link for "Read More..."
+      specialization: 'Oncology'
+    },
+    {
+      image: './asset/doc-2.jpg',
+      title: 'Pulmonologist',
+      description: 'Specialist in lung and respiratory diseases',
+      moreLink: './pulmonology-details.html', // Link for "Read More..."
+      specialization: 'Pulmonology'
+    },
+    {
+      image: './asset/doc-6.jpg',
+      title: 'Nephrologist',
+      description: 'Specialist in kidney health and diseases',
+      moreLink: './nephrology-details.html', // Link for "Read More..."
+      specialization: 'Nephrology'
+    },
+    {
+      image: './asset/doc-2.jpg',
+      title: 'Psychiatrist',
+      description: 'Focuses on mental health and behavioral disorders',
+      moreLink: './psychiatry-details.html', // Link for "Read More..."
+      specialization: 'Psychiatry'
+    }
       
   ];
   
