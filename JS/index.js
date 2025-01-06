46ab5e0ac4657ffcf6a586666ab10d8ef45e753b
CodeArtist-Muhammad-Tayyeb <22jzbcs0223@uetpeshawar.edu.pk>
2025-01-06 10:00:13 +0500

    upodate

--- JS/index.js
+++ JS/index.js
@@ -19,21 +19,21 @@
       image: './asset/doc-6.jpg',
       title: 'Cardiologist',
       description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum',
-      moreLink: './cardiology-details.html', // Link for "Read More..."
+      moreLink: './doctors-cards.html', // Link for "Read More..."
       speclization: 'MBBS'
     },
     {
       image: './asset/doc-2.jpg',
       title: 'Neurologist',
       description: 'Exploring the complexities of the human brain and nervous system',
-      moreLink: './neurology-details.html', // Link for "Read More..."
+      moreLink: './doctors-cards.html', // Link for "Read More..."
       speclization: 'Cardiologist'
     },
     {
       image: './asset/doc-6.jpg',
       title: 'Orthopedics',
       description: 'Specializing in the treatment of bones and joints',
-      moreLink: './orthopedics-details.html', // Link for "Read More..."
+      moreLink: './doctors-cards.html', // Link for "Read More..."
       speclization: 'Orthopedics'
     }
   ];
